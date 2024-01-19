0808438fd2f06280ea841d3fc22e4b879e52c267
alexmf91 <alexmunoz91@gmail.com>
2024-01-19 20:06:47 +0100

    Modificar componente Header: - Añadir pading para version mobile - Añadir sombra

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="flex px-8 py-4 border-b justify-between items-center bg-white">
+    <header className="flex py-2 px-3 sm:px-8 sm:py-4 border-b justify-between items-center bg-white shadow-xs">
       {/* Logo para mobile */}
       <img src="/blockmaker-small-logo.png" className="sm:hidden" alt="blockmaker-logo" width={45} />
       {/* Logo para desktop */}
