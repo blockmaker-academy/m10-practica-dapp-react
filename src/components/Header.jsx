1dc6a9d3e27803053411eb63b328b0ed965ee1da
alexmf91 <alexmunoz91@gmail.com>
2024-01-23 21:37:04 +0100

    Actualizar Header: - Añadir connectkit button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { ConnectKitButton } from 'connectkit'
+
 export default function Header() {
   return (
     <header className="flex py-2 px-3 sm:px-8 sm:py-4 border-b justify-between items-center bg-white shadow-xs">
@@ -5,7 +7,8 @@ export default function Header() {
       <img src="/blockmaker-small-logo.png" className="sm:hidden" alt="blockmaker-logo" width={45} />
       {/* Logo para desktop */}
       <img src="/blockmaker-full-logo.png" className="hidden sm:flex" alt="blockmaker-logo" width={250} />
-      <button className="bg-gray-200 rounded-md">Conectar Wallet</button>
+
+      <ConnectKitButton showBalance />
     </header>
   )
 }
